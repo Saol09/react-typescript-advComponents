e857fa7e264609dc69ffc6186c4a0f8eac2eb30f
Sérgio Lopes <saol09@gmail.com>
2023-12-11 10:58:11 +0000

    Updated with some new classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,16 @@
-import Input from './components/Input';
-import Form, { type FormHandle } from './components/Form';
-import Button from './components/Button';
-import { useRef } from 'react';
+import AddTimer from './components/AddTimer.tsx';
+import Header from './components/Header.tsx';
+import Timers from './components/Timers.tsx';
 
 function App() {
-  const customForm = useRef<FormHandle>(null);
-
-  function handleSave(data: unknown) {
-    const extractedData = data as { name: string; age: string };
-    console.log(extractedData);
-    customForm.current?.clear();
-  }
-
   return (
-    <main>
-      <Form onSave={handleSave} ref={customForm}>
-        <Input type="text" label="Name" id="name" />
-        <Input type="number" label="Age" id="age" />
-        <p>
-          <Button>Save</Button>
-        </p>
-      </Form>
-    </main>
+    <>
+      <Header />
+      <main>
+        <AddTimer />
+        <Timers />
+      </main>
+    </>
   );
 }
 
